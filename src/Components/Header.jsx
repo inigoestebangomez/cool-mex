57b33a7d07e4422c46f99effc123974ee1f138d0
Iñigo Esteban <nack_z4@hotmail.com>
2024-09-11 18:55:26 +0200

    header en proceso

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import header from '../images/header.jpg'
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='header'>
+      <div className='header-image'>
+      <span className='title-red'>COOL<span className='title-white'>-</span><span className='title-green'>MEX</span></span>
+      <div className='menu'>
+      <Link to= "/menu"><span>MENU</span></Link>
+      <Link to= "/menu"><span>HOURS & LOCATION</span></Link>
+      <Link to= "/menu"><span>OUR STORY</span></Link>
+      <Link to= "/menu"><span>RESERVATIONS</span></Link>
+      </div>
+      </div>
     </div>
   )
 }
