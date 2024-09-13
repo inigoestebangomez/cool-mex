6f33d791ef4008088e8e9497ca020acdf373a290
Iñigo Esteban <nack_z4@hotmail.com>
2024-09-13 14:36:18 +0200

    menu maquetado inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
+import Menu from './Pages/Menu'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Routes>
       <Route path= "/" element = {<Home />} />
+      <Route path= "/menu" element = {<Menu />} />
     </Routes>
     </>
   )
---
+++ src/Pages/Menu.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+
+function Menu() {
+  return (
+    <>
+      <Header />
+      <div className='menu-poster'>
+        <span className='menu-text'>MENU</span>
+      </div>
+      <div className='menu'>
+        <div className='menu-container'>
+        <h3>ENTRANTES</h3>
+        <ul className='menu-info'>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAA</li>
+        </ul>
+        </div>
+        <div className='menu-container'>
+        <h3>PLATO PRINCIPAL</h3>
+        <ul className='menu-info-main'>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAA</li>
+        </ul>
+        </div>
+        <div className='menu-container'>
+        <h3>POSTRES</h3>
+        <ul className='menu-info-main'>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAA</li>
+            <li>AAAAAAAAAAAA</li>
+        </ul>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Menu
